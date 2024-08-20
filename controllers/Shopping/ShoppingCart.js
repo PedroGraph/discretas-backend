2eebb82de3aca4b78ad933401be91bf56c3c7aae
Pmedinam <p.medina@sell2rent.com>
2024-08-20 01:07:21 -0500

    Adding new function to shoppingCart controller

--- controllers/Shopping/ShoppingCart.js
+++ controllers/Shopping/ShoppingCart.js
@@ -21,9 +21,10 @@ export class ShoppingController {
   getProductsFromShoppingCart = async (req, res) => {
     try {
       const { userId } = req.params;
+      if(!userId || userId === 'undefined') return res.status(400).json({ error: 'Missing userId' });
       const products = await this.shoppingModel.getProductsFromShoppingCart(userId);
       logger.info('Items obtained successfully');
-      res.status(200).json(products);
+      return res.status(200).json(products);
     } catch (error) {
       logger.error('Error obtaining items - Server error');
       res.status(500).json({ error: `Error server: the items could not be obtained. Error message: ${error}` });
@@ -31,41 +32,42 @@ export class ShoppingController {
   }
 
   updateProductIntoShoppingCart = async (req, res) => {
-    const shoppingCartId = req.params.id;
-    const updatedData = req.body;
-
+    const userId = req.params.id;
+    const {productId, quantity} = req.body;
+    console.log(req.body)
     try {
-      const updateShoppingCart = await this.shoppingModel.updateProductIntoShoppingCart({ updatedData, shoppingCartId });
+      const updateShoppingCart = await this.shoppingModel.updateProductQuantityInShoppingCart({ userId, productId, quantity });
 
       if (updateShoppingCart) {
-        logger.info(`Shopping cart with id ${shoppingCartId} has been updated successfully `);
+        logger.info(`Shopping cart with id ${productId} has been updated successfully `);
         res.status(200).json({ info: updateShoppingCart });
       }
 
-      logger.warn(`Shopping cart with id ${shoppingCartId} not found ${shoppingCartId}`);
+      logger.warn(`Shopping cart with id ${productId} not found ${productId}`);
       res.status(404).json({ error: ' Shopping cart not found' });
 
     } catch (error) {
-      logger.error(`Error updating shopping cart ${shoppingCartId} - Server error`);
-      res.status(500).json({ error: `Error server: shopping cart could not be updated. Error message. ${error}` });
+      logger.error(`Error updating shopping cart ${productId} - Server error`);
+      // res.status(500).json({ error: `Error server: shopping cart could not be updated. Error message. ${error}` });
     }
   }
 
   deleteShoppingCart = async (req, res) => {
-    const shoppingCartId = parseInt(req.params.shoppingCartId);
+    const userId = req.params.id;
+    const { productId } = req.query;
     try {
-      const deleteShoppingCart = await this.shoppingModel.deleteProductById(shoppingCartId);
+      const deleteShoppingCart = await this.shoppingModel.deleteProductFromShoppingCart({ userId, productId });
 
       if (deleteShoppingCart) {
-        logger.info(`Shopping cart with id ${shoppingCartId} has been deleted`);
-        res.status(204).json({ info: 'Shopping cart deleted' });
+        logger.info(`Shopping cart with id ${productId} has been deleted`);
+        return res.status(204).json({ info: 'Shopping cart deleted' });
       }
 
-      logger.warn(`Shopping cart with id ${shoppingCartId} not found `);
-      res.status(404).json({ error: 'Shopping cart not found' });
+      logger.warn(`Shopping cart with id ${productId} not found `);
+      return res.status(404).json({ error: 'Shopping cart not found' });
 
     } catch (error) {
-      logger.error(`Error deleting shopping cart ${shoppingCartId} - Server error`);
+      logger.error(`Error deleting shopping cart ${productId} - Server error`);
       res.status(500).json({ error: `Error server: shopping cart could not be deleted. Error message: ${error}` });
     }
   }
--- models/postgres/shoppingCart.js
+++ models/postgres/shoppingCart.js
@@ -28,16 +28,16 @@ export class ShoppingCartModel {
     
     addProductToShoppingCart = async ({ userId, products }) => {
         try {
-
             let shoppingCart = await ShoppingCart.findOne({ where: { userId } });
-
+    
             if (!shoppingCart) {
                 shoppingCart = await ShoppingCart.create({
                     userId, 
-                    totalAmount: 0, 
+                    totalAmount: 0,
+                    products: [], 
                 });
             }
-
+    
             const newProducts = products.map(product => ({
                 productID: product.productID,
                 price: product.price,
@@ -45,24 +45,36 @@ export class ShoppingCartModel {
                 size: product.size,
                 color: product.color,
             }));
-
-            const updatedProducts = shoppingCart.products.length > 0 ? [...shoppingCart.products, ...newProducts] : newProducts;
+    
+            const updatedProducts = shoppingCart.products.map(cartProduct => {
+                const newProduct = newProducts.find(product => product.productID === cartProduct.productID);
+                if (newProduct) {
+                    return {
+                        ...cartProduct,
+                        quantity: cartProduct.quantity + newProduct.quantity
+                    };
+                }
+                return cartProduct;
+            });
+    
+            const remainingNewProducts = newProducts.filter(product => !updatedProducts.find(cartProduct => cartProduct.productID === product.productID));
+            updatedProducts.push(...remainingNewProducts);
+    
             const updatedTotalAmount = this.calculateTotalAmount(updatedProducts);
-
-            console.log(updatedProducts);
-            console.log(updatedTotalAmount);
-
+    
             await shoppingCart.update({ 
                 products: updatedProducts,
                 totalAmount: updatedTotalAmount 
             });
-
+    
             return shoppingCart;
-
+    
         } catch (error) {
-            console.log(error);
+            console.error('Error updating shopping cart:', error);
+            throw new Error('Could not update shopping cart');
         }
     }
+    
 
     getProductsFromShoppingCart = async (userId) => {
         try {
@@ -73,53 +85,70 @@ export class ShoppingCartModel {
                 return null;
             }
 
-            return shoppingCart.products;
+            return shoppingCart.dataValues;
         } catch (error) {
             console.log(error);
         }
     }
 
-    updateProductIntoShoppingCart = async ({ userId, updatedData }) => {
-        try {
-            const [rowsUpdated, [updatedShoppingCart]] = await ShoppingCart.update(updatedData, {
-                where: { userId },
-                returning: true,
-            });
-
-            if (rowsUpdated > 0) {
-                return updatedShoppingCart;
-            } else {
-                console.log('Shopping cart was not updated');
-                return null;
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    deleteShoppingCart = async ({ userId, productID }) => {
+    updateProductQuantityInShoppingCart = async ({ userId, productId, quantity }) => {
         try {
             const shoppingCart = await ShoppingCart.findOne({ where: { userId } });
+    
+            if (!shoppingCart) {
+                console.log('Shopping cart not found');
+                return null;
+            }
+    
+            const updatedProducts = shoppingCart.products.map(product => {
+                if (product.productID === productId) {
+                    return {
+                        ...product,
+                        quantity: quantity
+                    };
+                }
+                return product;
+            });
+    
+            await shoppingCart.update({ products: updatedProducts });
+    
+            return shoppingCart;
+        } catch (error) {
+            console.error('Error updating product quantity:', error);
+            throw new Error('Could not update product quantity');
+        }
+    }
 
+    deleteProductFromShoppingCart = async ({ userId, productId }) => {
+        try {
+            const shoppingCart = await ShoppingCart.findOne({ where: { userId } });
+    
             if (!shoppingCart) {
                 console.log('User shopping cart not found');
                 return null;
             }
-
-            const updatedProducts = shoppingCart.products.filter(product => product.productID !== productID);
+    
+            const updatedProducts = shoppingCart.dataValues.products.filter(product => product.productID !== productId);
+            
+            if (updatedProducts.length === 0) {
+                await shoppingCart.destroy();
+                return null; // No products left in the shopping cart
+            }
+    
             const updatedTotalAmount = this.calculateTotalAmount(updatedProducts);
-
+    
             await shoppingCart.update({ 
                 products: updatedProducts,
                 totalAmount: updatedTotalAmount 
             });
-
+    
             return shoppingCart;
         } catch (error) {
-            console.log(error);
+            console.error('Error deleting product from shopping cart:', error);
+            throw new Error('Could not delete product from shopping cart');
         }
     }
-
+    
     calculateTotalAmount = (products) => {
         return products.reduce((total, product) => {
             return total + (product.price * product.quantity);
