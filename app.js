98a1e57a70ca0b231de48f1bff48fd95bbc6d12e
Pmedinam <p.medina@sell2rent.com>
2025-02-11 01:37:33 -0500

    Removing test

--- app.js
+++ app.js
@@ -27,11 +27,7 @@ export const mainApp = async (models) => {
   app.enable('trust proxy');
   app.use(express.json());
   app.use(cookieParser());
-  app.use(cors({
-    origin: 'http://localhost:4000',
-    credentials: true,
-    allowedHeaders: 'Content-Type,Authorization'
-  }));
+  app.use(cors());
 
   mainRoutes(app, models, redisClient);
 
